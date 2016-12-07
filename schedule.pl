b47ac1739d980f8a8fd9e309ac36fb31f744e0f7
DemonKiku <1043896394@qq.com>
2016-12-07 21:18:52 +0800

    input informations

---
+++ schedule.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl -w
+use strict;
+use utf8;
+
+#input infomations.
+my @bioth=('2016-12-16&2016-12-23','2016-12-30','2017-1-12');
+my @bioex=('No classroom discussion!','2016-12-12','2016-12-19');
+my @perlth=('2017-1-4','2017-1-4','2017-1-9');
+my @perlex=('No classroom discussion!','2017-1-5','No exam!');
+my @cbth=('2017-1-3','2017-1-3','2017-1-11');
+my @cbex=('No classroom discussion!','2016-12-9','2016-12-16','2016-12-23');
+my @pha=('No classroom discussion!','2017-1-4(Need to hand in your summary!)','2017-1-10');
+my @sp=('No classroom discussion!','2016-12-27','2017-1-3');
+
+print "@bioth","\n\n";
+print "@bioex\n\n";
+print "@perlth\n\n";
+print "@perlex\n\n";
+print "@cbth\n\n";
+print "@cbex\n\n";
+print "@pha\n\n";
+print "@sp\n\n";
